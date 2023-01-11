3d302e713014fc3063859a10eb31a2e8b5e55974
maxime-vds <maxime.vandriessche@hotmail.be>
2023-01-11 16:58:38 +0100

    first function for the cookie only the click in the img

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import "./index.css";
+
+
+
+(() => {
+// les variables
+let amountcookie= 0;
+let cookie= document.getElementById("imgcookie");
+let score= document.getElementById("pointcookie");
+
+// action suite au click
+cookie.addEventListener("click",() => {
+    amountcookie += 1; 
+    console.log (amountcookie);
+    score.textContent= ("le nombre de cookie" + amountcookie);
+});
+
+
+const cookieclicker= () => {
+
+    
+ 
+} 
+
+
+}) ()
