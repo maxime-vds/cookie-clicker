8e7a69b68f90dfa37f1830fae42298357a5cffa1
maxime-vds <maxime.vandriessche@hotmail.be>
2023-01-12 17:06:27 +0100

    js debut du code

--- src/main.js
+++ src/main.js
@@ -2,26 +2,98 @@ import "./index.css";
 (() => {
 
     //Declare asset values (ammounts and prices)
- 
+    
+    let score= 0;
+    let price= 20;
+    let multiplicateurCookie= 0;
+    let autoclcik= 0;
+    let timeleft=30;
+    let multi=0;
+     
+    
     
 
     //Declare the DOM elements 
+    let cookieImgButton= document.getElementById("cookie-img");
+    let multiplierButton= document.getElementById("multiplier");
+    let autoclickerButton= document.getElementById("autoclicker");
+    let boostButton= document.getElementById("boost");
    
 
     //Push the values in the DOM elements
-   
+    
+  
+
+
 
     //click the cookie to increment. (click event function)
+   cookieImgButton.addEventListener ("click", ()=> {
+    
+        multiplicateurCookie++;
+        console.log (multiplicateurCookie) ;
+        document.getElementById("cookieDisplay").innerHTML = multiplicateurCookie; 
+    });
    
 
     //click the Multiplier button to increment multiplier.     (click event function)
+    multiplierButton.addEventListener ("click", ()=> {
+    
+        multi+=1;
+        console.log (multi*5) ;
+        document.getElementById("cookieDisplay").innerHTML = multi*5 ;// pour associer le score mais souci d'affichage ( multi*5 + multiplicateurCookie); 
+        
+    });
+
 
     //click the Automaters button to increment automaters.     (click event function)
+   
+    autoclickerButton.addEventListener ("click", ()=> {
+      
+        function incrementScore() {
+            score++;
+            console.log(score);
+          }
+          let intervalId = setInterval(incrementScore, 1000);
+          setTimeout(function() {
+              clearInterval(intervalId);
+              console.log("Score final: " + score);
+            }, 20000);
+      
+        document.getElementById("cookieDisplay").innerHTML = score; //// pour associer le score mais souci d'affichage ( score+ multiplicateurCookie);
+        
+    });
+    
 
     //click the Booster button to to boost by 200% for 30 sec. (click event function)
+ 
+    boostButton.addEventListener ("click", ()=> {
+        
+    boost = 2;
+    intervalId = setInterval(incrementScore, 1000);
+    setTimeout(function() {
+    clearInterval(intervalId);
+    boost = 1;
+  }, 30000);
+    
+       
+       document.getElementById("cookieDisplay").innerHTML= boost; // pour associer le score mais souci d'affichage ( boost + multiplicateurCookie);
+    });
+  
+      
+
+
+
 
     //autoclciker function
 
-    //bonus booster function
 
+
+    //bonus booster function
+    let intervalId;
+    let boost= 1;
+    function incrementScore() {
+        score += boost;
+        console.log(score);
+      }
+            
 }) ()
