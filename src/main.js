4d3a8609c6312f8946a7aaf26b603e2d72645e0a
Buidlor <jefboone@gmail.com>
2023-01-12 11:20:15 +0100

    start

--- src/main.js
+++ src/main.js
@@ -2,39 +2,17 @@ import "./index.css";
 (() => {
 
     //Declare asset values (ammounts and prices)
-    let cookies = 100
-
-    let multiplier = {
-        amount: 1,
-        price : 100
-    }
-    let automater = {
-        amount: 1,
-        price : 100
-    }
-    let boostPrice = 100
+ 
     
 
     //Declare the DOM elements 
-    const cookieImg = document.getElementById('cookie-img')
-
-    const cookieDisplay = document.getElementById('cookieDisplay')
-    const multiplierBtn = document.getElementById('multiplier')
-    const automaterBtn = document.getElementById('autoclicker')
-    const boostBtn = document.getElementById('boost')
+   
 
     //Push the values in the DOM elements
-    cookieDisplay.innerHTML = `${cookies}`
-    multiplierBtn.innerHTML = `Multipliers: ${multiplier.amount}X <br> Price: ${multiplier.price}`
-    automaterBtn.innerHTML = `Automaters: ${automater.amount}<br> Price: ${automater.price}`
-    boostBtn.innerHTML = `200% booster <br> Price: ${boostPrice}`
+   
 
     //click the cookie to increment. (click event function)
-    cookieImg.addEventListener('click', () => {
-        cookies ++
-        console.log(cookies)
-        cookieDisplay.innerHTML =`${cookies}`
-    })
+   
 
     //click the Multiplier button to increment multiplier.     (click event function)
 
