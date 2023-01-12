0bb2763ff66132134d2fb2b197f7b09ff3a68c70
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-12 21:52:03 +0100

    work on booster function

--- src/main.js
+++ src/main.js
@@ -2,94 +2,89 @@ import "./index.css";
 (() => {
 
     //Declare asset values (ammounts and prices)
-    let cookies = 100
-
-    let multiplier = {
-        amount: 1,
-        price : 110
-    }
-    let automater = {
-        amount: 0,
-        price : 100
-    }
-    let boostPrice = 500
-   
-    setInterval(()=> {
-        cookies = cookies * automater.amount
-        cookieDisplay.innerHTML = `${cookies}`
-        return cookies
-   },1000)
-
+    let cookies = 0
+    let multiplier = {amount: 1, price: 10}
+    let automater =  {amount: 0, price: 10}
+    let boostPrice = 10
+    let booster = false
     
     //Declare the DOM elements 
     const cookieImg = document.getElementById('cookie-img')
-
     const cookieDisplay = document.getElementById('cookieDisplay')
     const multiplierBtn = document.getElementById('multiplier')
     const automaterBtn = document.getElementById('autoclicker')
     const boostBtn = document.getElementById('boost')
-
+    
     //Push the values in the DOM elements
+    const pushDom = () => {
+        cookieDisplay.innerHTML = `${Math.floor(cookies)}`
+        multiplierBtn.innerHTML = `Multipliers: ${Math.floor(multiplier.amount)}X <br> Price: ${Math.floor(multiplier.price)}`
+        automaterBtn.innerHTML = `Automaters: ${Math.floor(automater.amount)}<br> Price: ${Math.floor(automater.price)}`
+        boostBtn.innerHTML = `200% booster <br> Price: ${Math.floor(boostPrice)}`
+    } 
+    pushDom()
 
-
-    cookieDisplay.innerHTML = `${cookies}`
-    multiplierBtn.innerHTML = `Multipliers: ${multiplier.amount}X <br> Price: ${multiplier.price}`
-    automaterBtn.innerHTML = `Automaters: ${automater.amount}<br> Price: ${automater.price}`
-    boostBtn.innerHTML = `100% booster <br> Price: ${boostPrice}`
+    const checkPrice = (price) => {
+        if( cookies >= price ){
+            cookies = cookies - price
+            return true
+        }
+        else{
+            alert("not enough cookies")
+            return false
+        } 
+    }
+    
+    //every second add a cookie times the automaters to the balance
+    setInterval(()=> {
+        cookies = booster ? cookies + 1 * automater.amount*2 :  cookies + 1 * automater.amount
+        pushDom()
+    },1000)
 
     //click the cookie to increment. (click event function)
     cookieImg.addEventListener('click', () => {
-        cookies = calc()
-        cookieDisplay.innerHTML =`${cookies}`
-        return cookies
+        cookies = cookies + 1 * multiplier.amount
+        cookieDisplay.innerHTML =`${Math.floor(cookies)}`
     })
 
     //click the Multiplier button to increment multiplier.     (click event function)
     multiplierBtn.addEventListener("click", () => {
-        if( cookies >= multiplier.price ){
-            cookies = cookies - multiplier.price
+        if(checkPrice(multiplier.price)) {
             multiplier.amount ++
-            multiplier.price = Math.floor(multiplier.price) *1.2     
-            multiplierBtn.innerHTML = `Multipliers: ${multiplier.amount}X <br> Price: ${multiplier.price}`
-            cookieDisplay.innerHTML =`${cookies}`
+            multiplier.price *= 1.2
         }
-        else{
-            alert("not enough cookies")
-        }
-        
-        return multiplier
+        pushDom()
     })
 
     //click the Automaters button to increment automaters.     (click event function)
     automaterBtn.addEventListener('click', () => {
-        automater.amount ++
-        automater.price = Math.floor(automater.price) *1.2     
-        automaterBtn.innerHTML = `automaters: ${automater.amount}X <br> Price: ${automater.price}`
-        return automater
+        if(checkPrice(automater.price)){
+            automater.amount++
+            automater.price *= 1.2
+        }  
+        pushDom()
     })
 
     boostBtn.addEventListener('click', () => { 
-        console.log("click ")
-        boostPrice = Math.floor(boostPrice)*1.2
-        boostBtn.innerHTML = `100% booster <br> Price: ${boostPrice}`
-        return boostPrice
+        if(checkPrice(boostPrice)){
+            boostPrice *= 1.2 
+            let timer = 0
+            let boostInterval = setInterval(() => {
+                timer++
+                booster = true
+                console.log(timer, booster)  
+                if(timer === 10) {
+                    clearInterval(boostInterval)
+                    booster = false
+                }
+            }, 1000)
+            console.log(booster)
+        }
+        pushDom()
     })
 
-    const calc = () => {
-        cookies = cookies + 1 * multiplier.amount
-        console.log("calculation, ",  cookies)
-        return cookies
-        
-    }
-
- 
-
-   
 
     //click the Booster button to to boost by 200% for 30 sec. (click event function)
 
-    //autoclciker function
-
-    //bonus booster function
 
 }) ()
