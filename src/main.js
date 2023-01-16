fa6058f1dae7e54b7ec382cbd0f96f6e7da36e1d
maxime-vds <maxime.vandriessche@hotmail.be>
2023-01-16 09:26:20 +0100

    merge work team

--- src/main.js
+++ src/main.js
@@ -4,50 +4,118 @@ import "./index.css";
 
 (() => {
 
-/* style souris 
-function setCursorByID(mm,cursorStyle) {
-    var elem;
-    if (document.getElementById &&
-       (elem=document.getElementById(mm)) ) {
-     if (elem.style) elem.style.cursor= pointer;
+ //Declare assets with default values 
+ const defaultValues = () => {
+    let cookies = 0
+    let multiplier = {amount: 1, price: 10}
+    let automater  = {amount: 0, price: 10}
+    let boosters   = {amount: 0, price: 10, active: false}
+    return [cookies, multiplier, automater, boosters]
+}
+let [cookies, multiplier, automater, boosters] = defaultValues()  //destructuring defaultvalues() in variables
+
+
+//Declare the DOM elements 
+const cookieImg = document.getElementById('cookie-img')
+const cookieDisplay = document.getElementById('cookieDisplay')
+const multiplierBtn = document.getElementById('multiplier')
+const automaterBtn = document.getElementById('autoclicker')
+const boostBtn = document.getElementById('boost')
+const resetbtn = document.getElementById('resetbtn')
+
+
+//Push the values in the DOM elements. So that the values are being displayed on the webpage
+const pushDom = () => {
+    cookieDisplay.innerHTML = `${Math.floor(cookies)}`
+    multiplierBtn.innerHTML = `Multipliers: ${Math.floor(multiplier.amount)}X <br> Price: ${Math.floor(multiplier.price)}`
+    automaterBtn.innerHTML = `Automaters: ${Math.floor(automater.amount)}<br> Price: ${Math.floor(automater.price)}`
+    boostBtn.innerHTML = `200% booster <br> Price: ${Math.floor(boosters.price)}`
+} 
+pushDom()
+
+
+// Checks if you have enough cookies to perform a buy operation
+const checkPrice = (price) => {
+    if( cookies >= price ){
+        cookies = cookies - price
+        return true
     }
-   }*/
-   
-// les variables
-let amountcookie= 0;
-let cookie= document.getElementById("imgcookie");
-let score= document.getElementById("pointcookie");
-
-// action suite au click
-cookie.addEventListener("click",() => {
-    amountcookie += 1; 
-    console.log (amountcookie);
-    score.textContent= ("le nombre de cookie : " + amountcookie);
-});
+    else{
+        alert("not enough cookies")
+        return false
+    } 
+}
 
 
-const cookieclicker= (amontcookie) => {
-    //button multiplicateur
-    /*   
-    let multibutton = document.getElementById ("multybutton");
-    
-    multibutton.addEventListener ("click", ()=> {
-    let multyamontcookie= amontcookie * 2;
-    console.log (multyamontcookie);
-    
-    });
-    
-    
-    */ 
-
-    //button autoclick
+//calcuclates the increment rate
+const incrementer = (assetAmount) => {
+    cookies = boosters.active ? cookies + 1 * assetAmount*3 :  cookies + 1 * assetAmount
+    return cookies
+}
 
 
-    //button (bonus) boost
+//Every second add a cookie times the automaters to the balance
+setInterval(()=> {
+    incrementer(automater.amount)
+    pushDom()
+},1000)
 
+
+//click the cookie to increment. 
+cookieImg.addEventListener('click', () => {
+   incrementer(multiplier.amount)
+   pushDom()
+})
+
+
+//click the Multiplier button to increment multiplier.     
+multiplierBtn.addEventListener("click", () => {
+    if(checkPrice(multiplier.price)) {
+        multiplier.amount ++
+        multiplier.price *= 1.1
+    }
+    pushDom()
+})
+
+
+//click the Automaters button to increment automaters.     
+automaterBtn.addEventListener('click', () => {
+    if(checkPrice(automater.price)){
+        automater.amount ++
+        automater.price *= 1.1
+    }  
+    pushDom()
+})
+
+
+//Booster function. When clicked, you are boosted for 10 seconds. The boost gives you 2x cookies on the cookieclick and 2x cookies from the automaters. 
+//Before activating the booster, the function checks if the booster is already active or not.     
+boostBtn.addEventListener('click', () => { 
+    if(cookies >= boosters.price && boosters.active  === false){
+        cookies =cookies - boosters.price
+        boosters.price *= 1.1 
+        boosters.active  = true
+        let timer = 10
+        let boostInterval = setInterval(() => {
+            timer--
+            console.log(timer, boosters.active )  
+            if(timer <= 0) {
+                clearInterval(boostInterval)
+                boosters.active  = false
+            }
+        }, 1000)            
+    }
+    else { console.log("booster is already active or not enough coockies", boosters.active )}
+    pushDom()
+})
+
+//reset Button, sets all variables to default value
+// resetbtn.addEventListener('click',() => {
+
+// })
     
  
-} 
+
 
 
 }) ()
