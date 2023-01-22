542d0d3ba081a3df12cf2156f977b7064aab6ec3
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-22 16:20:49 +0100

    backend routes and modals for the cookieassets

--- server/model/model.js
+++ server/model/model.js
@@ -1,13 +1,51 @@
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
-    name: {
+    user: {
         required: true,
         type: String
     },
-    age: {
-        required: true,
-        type: Number
+    totalcookies: {
+        type: Number,
+        default: 0
+    },
+    cookies: {
+        type: Number,
+        default: 0
+    },
+    multiplier: {
+        amount:{
+            type:Number,
+            default: 1
+        },
+        price:{
+            type: Number,
+            default:10
+        }
+    },
+    automater: {
+        amount:{
+            type: Number,
+            default: 0
+        },
+        price:{
+            type:Number,
+            default:10
+        }
+    },
+    boosters: {
+        amount:{
+            type: Number,
+            default: 0
+        },
+        price:{
+            type: Number,
+            default:10
+        },
+        active:{
+            type: Boolean,
+            default: false
+        }
     }
 })
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,13 +4,17 @@ const Model = require('../model/model');
 module.exports = router;
 
 //Post Method
-router.post('/post', (req, res) => {
-    const data =new Model({
-        name: req.body.name,
-        age: req.body.age
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        user: req.body.user,
+        totalcookies: req.body.totalcookies,
+        cookies: req.body.cookies,
+        multiplier: req.body.multiplier,
+        automater: req.body.automater,
+        boosters: req.body.boosters
     })
     try{
-        const dataToSave = data.save();
+        const dataToSave = await data.save();
         res.status(200).json(dataToSave)
     }
     catch(error){
@@ -20,21 +24,52 @@ router.post('/post', (req, res) => {
 })
 
 //Get all Method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/getAll', async (req, res) => {
+   try{
+        const data = await Model.find();
+        res.json(data)
+   }
+   catch(error){
+        res.status(500).json({message: error.message})
+   }
 })
 
 //Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/getOne/:id', async (req, res) => {
+   try{
+    const data = await Model.findById(req.params.id)
+    res.json(data)
+   }
+   catch(error){
+    res.status(500).json({message: error.message})
+   }
 })
 
 //Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedData = req.body
+        const options = {new:true}
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+        res.send(result)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.user} has been deleted ...`)
+    }
+    catch{
+        res.status(400).json({message: error.message})
+    }
 })
