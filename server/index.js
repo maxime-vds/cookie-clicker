2eb456ec2dd6849c89c06ae5cbdd319edd0d1b5f
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-22 12:49:03 +0100

    working on backend

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const routes = require('./routes/routes')
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL //set url
+
+
+mongoose.set('strictQuery', false); //removes the wwarning
+mongoose.connect(mongoString, {useNewUrlParser: true, useUnifiedTopology: true})
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+database.once('connected', () => {
+    console.log('Database Connected')
+})
+const app = express()
+
+app.use(express.json())
+app.use('/api', routes)
+
+app.listen(3000, () => {
+    console.log(`server started at ${3000}`)
+})
